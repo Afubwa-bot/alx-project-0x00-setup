145bb5557e7c2777a3075b9204d6f8db873e6b8a
Afubwa. <emmanelafubwa@gmail.com>
2025-07-23 13:40:32 +0300

    Basic routing - Add About and Landing page components

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,8 @@
+const Landing: React.FC = () => {
+    return (
+        <div>
+            <h1  className="text-x1 font-extralight text-center"> Landing Page</h1>
+        </div>
+    );
+}
+export default Landing;
